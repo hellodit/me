a22d558bad91388885a34f4502bd4eecea45e8a5
hellodit <asditap@gmail.com>
2026-02-01 14:41:08 +0700

    change w header

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -5,7 +5,7 @@ import { cn } from '@/utils/css'
 
 export function Header({ className }: { className?: string }) {
   return (
-    <header className={cn('group relative z-50 flex items-center justify-between mx-auto h-16 px-4 max-w-2xl bg-transparent sm:px-6 lg:px-8', className)}>
+    <header className={cn('group relative z-50 flex items-center justify-between mx-auto h-16 px-4 max-w-3xl bg-transparent sm:px-6 lg:px-8', className)}>
       <Link href="/" className="flex items-center gap-1 overflow-hidden select-none shrink-0">
         <span className="relative z-[1] shrink-0 bg-white">
           <Logo className="size-5 fill-accent" />
